88069b5545f3fab827e853d5961cabae03d28a7f
Ululette <edithlosada@gmail.com>
2021-05-07 17:33:07 -0300

    1ra version footer

--- client/src/components/LandingP/LandingP.jsx
+++ client/src/components/LandingP/LandingP.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './LandingP.css';
+import Footer from '../Footer/Footer';
 //import de cada componente
 
 export default function LandingP() {
@@ -7,6 +8,7 @@ export default function LandingP() {
       <div >
         <h1> Integra salud</h1>
         <p> 'Acá agregamos los componentes por separado importandolos'</p>
+        <Footer/>
       </div>
     )
 }
