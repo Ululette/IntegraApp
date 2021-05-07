f68923b6a3f6b99286fbe8f990b01ff1bb21f53e
leon <leon.18@live.com.ar>
2021-05-07 16:21:02 -0300

    form contacto hecho

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import LandingP from './LandingP/LandingP.jsx';
+import ContactFormGuest from './contact_form_guest/contact_form_guest.jsx';
 
 function App() {
     return (
         <React.Fragment>
-            <Route exact path='/' component={LandingP} />
+            <Route exact path='/' component={ContactFormGuest} />
         </React.Fragment>
     );
 }
