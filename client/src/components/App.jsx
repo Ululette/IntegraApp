53c5961549966ba34f90b34797ab9d3d6716ee6f
leon <leon.18@live.com.ar>
2021-05-09 16:48:31 -0300

    terminado,falta testearlo

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import LandingP from './LandingP/LandingP.jsx';
-import ContactFormGuest from './contact_form_guest/contact_form_guest.jsx';
+import ContactForm from './ContactForm/ContactForm.jsx';
 
 function App() {
     return (
         <React.Fragment>
-            <Route exact path='/' component={ContactFormGuest} />
+            <Route exact path='/' component={ContactForm} />
         </React.Fragment>
     );
 }
--- client/src/components/ContactForm/ContactForm.jsx
+++ client/src/components/ContactForm/ContactForm.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
-import Styles from './contact_form_guest.module.css'
+import Styles from './ContactForm.module.css'
 
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-// import Typography from '@material-ui/core/Typography';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 import Snackbar from '@material-ui/core/Snackbar';
 import Alert from '@material-ui/lab/Alert';
-
 import Card from '@material-ui/core/Card';
 import { makeStyles } from '@material-ui/core/styles';
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://qeubfsxlcvapzvjihzep.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMDMyNDU4NCwiZXhwIjoxOTM1OTAwNTg0fQ.l9ZzKLUoPFsMWMCismH6RkXsEzBiSrDMylGB9V_HHjI'
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 // const LOGOLOCAL = 'client/src/components/contact_form_guest/logo-integra.png';
 const LOGO = "https://cdn.discordapp.com/attachments/837738450736250918/839883762019860490/manos1.png";
@@ -28,7 +32,6 @@ const theme = createMuiTheme({
 
 const useStyles = makeStyles({
     root: {
-    //   minWidth: 275,
         width:'50%',
         height:'70%',
         background:'#e8ffff'
@@ -46,54 +49,84 @@ const useStyles = makeStyles({
     },
   })
 
-function ContactFormGuest(){
+function ContactForm(){
     const classes = useStyles();
 
+    const[input,setInput] = useState({name:'',age:'',dni:'',phone_number:'',mail:''});
+    const[errors,setErrors] = useState({name:false,age:false,dni:false,phone_number:false,mail:false});
     const[openGood, setOpenGood] =useState(false);
     const[openBad, setOpenBad] =useState(false);
-    const[name,setName] = useState('');
-    const[old,setOld] = useState('');
-    const[dni,setDni] = useState('');
-    const[phone,setPhone] = useState('');
-    const[mail,setMail] = useState('');
-    
-    const mailPattern = /[a-zA-Z0-9]+[\.]?([a-zA-Z0-9]+)?[\@][a-z]{3,9}[\.][a-z]{2,5}/g;
-    const namePattern = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/g;
-    const numberPattern = /^\d*$/;
-    const mailResult = mailPattern.test(mail)
-    const nameResult = namePattern.test(name)
-    const oldResult = numberPattern.test(old)
-    const dniResult = numberPattern.test(dni)
-    const phoneResult = numberPattern.test(phone)
 
-    const handleClickOpen = () => {
-        if(nameResult && oldResult && dniResult && phoneResult && mailResult){
+    const handleClickOpen = async() => {
+        if(!errors.age && !errors.dni && !errors.phone_number && !errors.mail && !errors.name){
             setOpenGood(true);
+            const { data, error } = await supabase
+            .from('request_form')
+            .insert([
+                { name: input.name, age: input.age, dni: input.dni, phone_number: input.phone_number, mail: input.mail},
+            ])
+            console.log(error)
+            console.log(data)
         } else {
             setOpenBad(true);
         }
+        setInput({name:'',age:'',dni:'',phone_number:'',mail:''});
     };
-  
+    
     const handleClose = () => {
         setOpenGood(false);
         setOpenBad(false);
     };
 
-    const handleNameChange = (event) => {
-        setName(event.target.value);
+    const handleInputChange = (e) => {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        });
+        setErrors(
+            validate({
+                ...input,
+                [e.target.name]: e.target.value
+            })
+        );
     }
-    const handleOldChange = (event) => {
-        setOld(event.target.value);
-    }
-    const handleDniChange = (event) => {
-        setDni(event.target.value);
-    }
-    const handlePhoneChange = (event) => {
-        setPhone(event.target.value);
-    }
-    const handleMailChange = (event) => {
-        setMail(event.target.value);
-    }
+
+    function validate(input) {
+        const mailPattern = /[a-zA-Z0-9]+[\.]?([a-zA-Z0-9]+)?[\@][a-z]{3,9}[\.][a-z]{2,5}/g;
+        const namePattern = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/g;
+        const numberPattern = /^\d*$/;
+        let errors = {};
+        if (!namePattern.test(input.name)) {
+            errors.name = true;
+        } else{
+            errors.name = false;
+        }
+
+        if (!numberPattern.test(input.age)) {
+            errors.age = true;
+        } else{
+            errors.age = false;
+        }
+
+        if (!numberPattern.test(input.dni)&&(input.dni.length===10)) {
+            errors.dni = true;
+        } else{
+            errors.dni = false;
+        }
+
+        if (!numberPattern.test(input.phone_number)&&(input.phone_number.length<=12)) {
+            errors.phone_number = true;
+        } else{
+            errors.phone_number = false;
+        }
+
+        if (!mailPattern.test(input.mail)) {
+            errors.mail = true;
+        } else{
+            errors.mail = false;
+        }
+        return errors;
+    };
 
     return(
         <div className={Styles.conteinerAll}>
@@ -110,7 +143,6 @@ function ContactFormGuest(){
                     </Alert>
                 </Snackbar>
                 <div className={Styles.formConteiner}>
-                    
                     <div className={Styles.inputs}>
                         <div className={Styles.imgConteiner}>
                             <img src={LOGO} alt="" />
@@ -120,19 +152,23 @@ function ContactFormGuest(){
                             <TextField
                                 id="name-input"
                                 type="text"
+                                name="name"
                                 autoComplete='off'
-                                value={name}
-                                onChange={(e) => handleNameChange(e)}
+                                value={input.name}
+                                onChange={(e) => handleInputChange(e)}
+                                {...(errors.name && {error:errors.name,helperText:'Nombre invalido'})}
                                 />
                         </div>
                         <div className={Styles.textField}>
                             <label htmlFor="">Tengo </label>
                             <TextField
-                                id="old-input"
+                                id="age-input"
                                 type="tel"
+                                name="age"
                                 autoComplete='off'
-                                value={old}
-                                onChange={(e) => handleOldChange(e)}
+                                value={input.age}
+                                onChange={(e) => handleInputChange(e)}
+                                {...errors.age && {error:true,helperText:'Edad invalido'}}
                                 inputProps={{ maxLength: 3 }}
                             />
                             <label htmlFor=""> años.</label>
@@ -142,9 +178,11 @@ function ContactFormGuest(){
                             <TextField
                                 id="dni-input"
                                 type="tel"
+                                name="dni"
                                 autoComplete='off'
-                                value={dni}
-                                onChange={(e) => handleDniChange(e)}
+                                value={input.dni}
+                                onChange={(e) => handleInputChange(e)}
+                                {...(errors.dni && {error:true,helperText:'Dni invalido'})}
                                 inputProps={{ maxLength: 8 }}
                             />
                         </div>
@@ -153,9 +191,11 @@ function ContactFormGuest(){
                             <TextField
                                 id="phone-input"
                                 type="tel"
+                                name="phone_number"
                                 autoComplete='off'
-                                value={phone}
-                                onChange={(e) => handlePhoneChange(e)}
+                                value={input.phone_number}
+                                onChange={(e) => handleInputChange(e)}
+                                {...(errors.phone_number && {error:true,helperText:'Teléfono invalido'})}
                                 inputProps={{ maxLength: 12 }}
                             />
                         </div>
@@ -164,10 +204,11 @@ function ContactFormGuest(){
                             <TextField
                                 id="mail-input"
                                 type="email"
+                                name="mail"
                                 autoComplete='off'
-                                value={mail}
-                                onChange={(e) => handleMailChange(e)}
-                                inputProps={{ maxLength: 40 }}
+                                value={input.mail}
+                                onChange={(e) => handleInputChange(e)}
+                                {...(errors.mail && {error:true,helperText:'Mail invalido'})}
                             />
                         </div>
                     </div>
@@ -177,7 +218,6 @@ function ContactFormGuest(){
                             color="primary" 
                             style={{ borderRadius: 100 , margin: 10 }} 
                             onClick={handleClickOpen} 
-                            disabled={!(nameResult && oldResult && dniResult && phoneResult && mailResult)}
                         >
                             Consultar
                         </Button>
@@ -192,4 +232,4 @@ function ContactFormGuest(){
     );
 }
 
-export default ContactFormGuest;
+export default ContactForm;
