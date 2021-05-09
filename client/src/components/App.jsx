ba8ccd1dd26badeaac7ba8855e5459146541d221
Julian Vazquez <julianvazquez042@gmail.com>
2021-05-09 11:35:16 -0300

    merge with footer

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import LandingPage from './LandingPage/LandingPage';
 import NavBar from './NavBar/navBar.jsx';
+import Footer from './Footer/Footer.jsx';
 
 function App() {
     return (
         <React.Fragment>
             <Route path='/' component={NavBar} />
             <Route exact path='/' component={LandingPage} />
+            <Route path='/' component={Footer} />
         </React.Fragment>
     );
 }
