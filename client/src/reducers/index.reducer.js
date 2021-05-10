8305a6ce52fa1e2a19665fa197d46d303a88b7f7
Ululette <edithlosada@gmail.com>
2021-05-10 16:14:05 -0300

    actions faltantes

--- client/src/reducers/index.reducer.js
+++ client/src/reducers/index.reducer.js
@@ -1,7 +1,15 @@
 import { GET_PLANS_BENEFITS } from '../actions/constants.actions';
+import {
+  SET_NP_BEN_SEL, ADD_NP_BEN, SEND_NP_FORM
+} from '../actions/plans.actions';
 
 const initialState = {
     allPlans: [],
+  //---------New plan---
+  npbensel: [],
+  addedbenefs: [],
+  sended: false,
+  //-------------------
 };
 
 function rootReducer(state = initialState, action) {
@@ -11,6 +19,22 @@ function rootReducer(state = initialState, action) {
                 ...state,
                 allPlans: action.payload,
             };
+        //------New Plan------
+				case SET_NP_BEN_SEL:
+				  return {
+				    ...state,
+				    npbensel: action.payload
+				  }
+				case ADD_NP_BEN:
+				  return {
+				    ...state,
+				    addedbenefs: action.payload
+				  }
+				case SEND_NP_FORM:
+				  return {
+				    ...state,
+				    sended: action.payload
+				  }
         default:
             return state;
     }
