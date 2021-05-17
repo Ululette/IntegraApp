c81492b0d7ba14a4e18c92d500ee32a180726181
Julian Vazquez <julianvazquez042@gmail.com>
2021-05-17 14:48:33 -0300

    Update AffiliateDoctors.jsx

--- client/src/components/AffiliateDoctors/AffiliateDoctors.jsx
+++ client/src/components/AffiliateDoctors/AffiliateDoctors.jsx
@@ -1,7 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import supabase from '../../supabase.config';
+import React from 'react';
 import PropTypes from 'prop-types';
-// import SwipeableViews from 'react-swipeable-views';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
