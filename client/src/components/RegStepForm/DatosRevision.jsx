e6e462ca550c0854bd29855606f9e419b2358f88
Alan Diaz <Alanfdiaz@gmail.com>
2021-05-17 10:44:54 -0300

    Update DatosRevision.jsx
    Datos revision

--- client/src/components/RegStepForm/DatosRevision.jsx
+++ client/src/components/RegStepForm/DatosRevision.jsx
@@ -3,8 +3,37 @@ import React from "react"
 
 const DatosRevision = () =>{
 
-  return (
-    <p>soy un no form revision</p>
-  )
+return (
+  <div>
+  <div>
+    <h4>Revision de Datos</h4>
+  
+    <h5>Datos del Titular</h5>
+  
+    <label>Nombre</label>
+    <label>Apellido</label>
+    <label>Genero</label>
+    <label>DNI</label>
+    <label>CUIL</label>
+    <label>Teléfono</label>
+    <label>Email</label>
+    <label>Ocupacion</label>
+    <label>Estado civil</label>
+    <label>Fecha de nacimiento</label>
+    <label>Dirección</label>
+    <label>Calle</label>
+    <label>Numero</label>
+    <label>Piso/Depto</label>
+    <label>Provincia</label>
+    <label>Localidad</label>
+  </div>
+  <div>
+    <label>Razón Social</label>
+    <label>Grupo empresarial</label>
+    <label>Nombre y apellido del Referente de RRHH</label>
+    <label>Teléfono</label>
+    <label>E-mail</label>
+  </div>
+  </div>)
 }
 export default DatosRevision
