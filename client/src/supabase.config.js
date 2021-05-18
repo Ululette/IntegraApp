e1c538fa0c67e3804d81e766760cfb7c3dc7cf4f
Julian Vazquez <julianvazquez042@gmail.com>
2021-05-18 08:56:10 -0300

    Delete files with security problems

--- client/src/supabase.config.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://qeubfsxlcvapzvjihzep.supabase.co';
-const SUPABASE_KEY =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMDMyNDU4NCwiZXhwIjoxOTM1OTAwNTg0fQ.l9ZzKLUoPFsMWMCismH6RkXsEzBiSrDMylGB9V_HHjI';
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-
-export default supabase;
