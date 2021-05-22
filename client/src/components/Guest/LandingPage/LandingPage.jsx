61578074a45eceba4b1bb2fd93ae0c9cb19871b3
Julian Vazquez <julianvazquez042@gmail.com>
2021-05-22 19:05:35 -0300

    finish landing

--- client/src/components/Guest/LandingPage/LandingPage.jsx
+++ client/src/components/Guest/LandingPage/LandingPage.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './LandingPage.module.css';
 import InfoPlanes from '../InfoPlans/InfoPlanes.jsx';
 import ContactForm from '../ContactForm/ContactForm.jsx';
 import Carousel from './Carousel.jsx';
 import StarsBackground from './StarsBackground.jsx';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 export default function LandingPage() {
+    useEffect(() => {
+        Aos.init({ duration: 2000 });
+    }, []);
+
     return (
         <div className={styles.all}>
-            <header className={styles.first}>
+            <header data-aos='zoom-in' className={styles.first}>
                 <div className={styles.typewriter}>
                     <h2 className={styles.welcome}>Bienvenido a</h2>
                     <h2 className={styles.welcome2}>Integra Salud</h2>
@@ -23,7 +29,7 @@ export default function LandingPage() {
                     ></video>
                 </div>
             </header>
-            <section className={styles.promotions}>
+            <section data-aos='zoom-out' className={styles.promotions}>
                 <div className={styles.iconsScreen2}>
                     <div className={styles.icon1}>
                         <img
@@ -53,12 +59,32 @@ export default function LandingPage() {
                     </div>
                 </div>
                 <h1 className={styles.bigTitlePromotions}>
-                    <div className={styles.title1}>Tu mejor opcion</div>
-                    <div className={styles.title2}>Tu Prepaga</div>
-                    <div className={styles.title3}>Integra Salud</div>
+                    <div
+                        data-aos='zoom-in'
+                        data-aos-duration='1000'
+                        className={styles.title1}
+                    >
+                        Tu mejor opcion
+                    </div>
+                    <div
+                        data-aos='zoom-in'
+                        data-aos-duration='1000'
+                        data-aos-delay='400'
+                        className={styles.title2}
+                    >
+                        Tu Prepaga
+                    </div>
+                    <div
+                        data-aos='zoom-in'
+                        data-aos-duration='1000'
+                        data-aos-delay='800'
+                        className={styles.title3}
+                    >
+                        Integra Salud
+                    </div>
                 </h1>
             </section>
-            <section className={styles.promotions2}>
+            <section data-aos='fade-down' className={styles.promotions2}>
                 <div className={styles.promotions2_titles}>
                     <h3>Documentos virtuales</h3>
                     <h3>Todo al alcance de tu mano</h3>
@@ -79,41 +105,62 @@ export default function LandingPage() {
                     </h4>
                 </div>
             </section>
-            <section className={styles.carrouselNews}>
+            <section data-aos='zoom-out-down' className={styles.carrouselNews}>
                 <h3 className={styles.nov}>Novedades</h3>
                 <article className={styles.news}>
                     <Carousel />
                 </article>
             </section>
 
-            <section className={styles.digital}>
+            <section data-aos='zoom-in-left' className={styles.digital}>
                 <StarsBackground />
                 <h2 className={styles.digitalTitle}>
-                    <div>Credencial digital</div>
-                    <div className={styles.digitalSubtitle}>
+                    <div data-aos='zoom-in' data-aos-duration='1000'>
+                        Credencial digital
+                    </div>
+                    <div
+                        data-aos='zoom-in'
+                        data-aos-duration='1000'
+                        data-aos-delay='500'
+                        className={styles.digitalSubtitle}
+                    >
                         Innovamos para estar cada dia mas cerca tuyo!
                     </div>
                 </h2>
                 <div className={styles.digitalDescriptions}>
-                    <p className={styles.digitalDescription1}>
+                    <p
+                        data-aos='zoom-in'
+                        data-aos-duration='1000'
+                        data-aos-delay='750'
+                        className={styles.digitalDescription1}
+                    >
                         📱 La credencial va siempre con vos y sin ocupar lugar
                         en tu billetera.
                     </p>
-                    <p className={styles.digitalDescription2}>
+                    <p
+                        data-aos='zoom-in'
+                        data-aos-duration='1000'
+                        data-aos-delay='750'
+                        className={styles.digitalDescription2}
+                    >
                         📱Todas las credenciales de tu grupo familiar en un solo
                         lugar.
                     </p>
                 </div>
             </section>
-            <section className={styles.ourPlans}>
+            <section data-aos='slide-left' className={styles.ourPlans}>
                 <InfoPlanes id='infoPlans' />
             </section>
-            <section className={styles.contactSection}>
+            <section
+                id='contact'
+                data-aos='zoom-out-up'
+                className={styles.contactSection}
+            >
                 <div className={styles.titlesContact}>
-                    <h1>Estas a un solo paso</h1>
-                    <h2>de ser parte de Integra Salud</h2>
+                    <h1 data-aos='flip-down'>Estas a un solo paso</h1>
+                    <h2 data-aos='flip-down'>de ser parte de Integra Salud</h2>
                 </div>
-                <ContactForm />
+                <ContactForm data-aos='zoom-in' />
             </section>
         </div>
     );
