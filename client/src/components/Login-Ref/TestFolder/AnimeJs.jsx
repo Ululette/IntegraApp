8134d641770466927512fb348ef1ae46aa9050d9
Julian Vazquez <julianvazquez042@gmail.com>
2021-05-23 14:31:42 -0300

    minor changes

--- client/src/components/Login-Ref/TestFolder/AnimeJs.jsx
+++ client/src/components/Login-Ref/TestFolder/AnimeJs.jsx
@@ -1,33 +1,33 @@
-import React, { useEffect } from 'react';
-import anime from 'animejs';
-import './AnimeJs.css';
+// import React, { useEffect } from 'react';
+// import anime from 'animejs';
+// import './AnimeJs.css';
 
-function AnimeJs() {
-    function randomValues() {
-        anime({
-            targets: 'square',
-            translateX: function () {
-                return anime.random(0, 500);
-            },
-            easing: 'easeInOutQuad',
-            duration: 750,
-            complete: randomValues,
-        });
-    }
+// function AnimeJs() {
+//     function randomValues() {
+//         anime({
+//             targets: 'square',
+//             translateX: function () {
+//                 return anime.random(0, 500);
+//             },
+//             easing: 'easeInOutQuad',
+//             duration: 750,
+//             complete: randomValues,
+//         });
+//     }
 
-    useEffect(() => {
-        randomValues();
-    });
+//     useEffect(() => {
+//         randomValues();
+//     });
 
-    return (
-        <div className='container'>
-            <div className='square'></div>
-            <div className='square'></div>
-            <div className='square'></div>
-            <div className='square'></div>
-            <div className='square'></div>
-        </div>
-    );
-}
+//     return (
+//         <div className='container'>
+//             <div className='square'></div>
+//             <div className='square'></div>
+//             <div className='square'></div>
+//             <div className='square'></div>
+//             <div className='square'></div>
+//         </div>
+//     );
+// }
 
-export default AnimeJs;
+// export default AnimeJs;
--- client/src/components/Routes/RoutesLogin.jsx
+++ client/src/components/Routes/RoutesLogin.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { useFirebaseApp } from 'reactfire';
 import Login from '../Login-Ref/Login/Login.jsx';
-import AnimateJs from '../Login-Ref/TestFolder/AnimeJs.jsx';
+// import AnimateJs from '../Login-Ref/TestFolder/AnimeJs.jsx';
 
 function RoutesLogin() {
     const firebase = useFirebaseApp();
@@ -13,7 +13,7 @@ function RoutesLogin() {
                 path='/login'
                 render={() => <Login firebase={firebase} />}
             />
-            <Route exact path='/animatejs' component={AnimateJs} />
+            {/* <Route exact path='/animatejs' component={AnimateJs} /> */}
         </>
     );
 }
