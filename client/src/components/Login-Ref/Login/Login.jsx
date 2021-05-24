20c4acd2aba4dc921b56226fde7201a2d47a13c5
Julian Vazquez <julianvazquez042@gmail.com>
2021-05-24 11:12:30 -0300

    merge with seba

--- client/src/components/Login-Ref/Login/Login.jsx
+++ client/src/components/Login-Ref/Login/Login.jsx
@@ -25,14 +25,6 @@ function Login({ firebase }) {
     const userFire = useUser();
 
     const userData = JSON.parse(localStorage.getItem('userdata'));
-    // if (userData && userData.role === 'admin' && userFire.data)
-    //     window.location = `/${userData.dni}/admin`;
-
-    // if (userData && userData.role === 'medic' && userFire.data)
-    //     window.location = `/${userData.dni}/medic`;
-
-    // if (userData && userData.role === 'affiliate' && userFire.data)
-    //     window.location = `/${userData.dni}/affiliate`;
 
     const handleChange = (event) => {
         const value = event.target.value;
@@ -64,130 +56,122 @@ function Login({ firebase }) {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        //buscar en la base si esta activo el user
-        //informar en caso de que este inactivo
-
         setInput({ doc: '', pass: '' });
         setLoading(true);
         try {
             let { data: users, error } = await supabase
                 .from('users')
-                .select('dni, email, role, avatar_url,account')
+                .select('dni, email, role, avatar_url')
                 .eq('dni', input.doc);
             if (error) return console.log(error);
-            if (users[0].account === 'inactive') {
-                Swal.fire({
-                    title: `Error!`,
-                    text: `Access denied! Your account is inactive. Contact the administrator`,
-                    icon: 'error',
-                    confirmButtonText: 'OK',
-                });
+            const numRole =
+                users[0].role === 'affiliate'
+                    ? 10
+                    : users[0].role === 'medic'
+                    ? 20
+                    : 30;
+            if (numRole !== role) {
                 setLoading(false);
+                return setErrors('Usuario y/o contraseña incorrecto.');
             } else {
-                const numRole =
-                    users[0].role === 'affiliate'
-                        ? 10
-                        : users[0].role === 'medic'
-                        ? 20
-                        : 30;
-                if (numRole !== role) {
-                    setLoading(false);
-                    return setErrors('Usuario y/o contraseña incorrecto.');
-                } else {
-                    setErrors(null);
-                }
-                if (users[0].role === 'affiliate') {
-                    let { data: userInfo, error: errorFetchUserInfo } =
-                        await supabase
-                            .from(`partners`)
-                            .select(
-                                'name, lastname, family_group, plans (id, name)'
-                            )
-                            .eq('dni', users[0].dni);
-
-                    if (errorFetchUserInfo) {
-                        console.log(errorFetchUserInfo);
-                        setLoading(false);
-                        return alert('Error en fetch user info.');
-                    }
-
-                    const affiliateData = {
-                        name: userInfo[0].name,
-                        lastname: userInfo[0].lastname,
-                        plan_id: userInfo[0].plans.id,
-                        plan_name: userInfo[0].plans.name,
-                        family_group: userInfo[0].family_group,
-                    };
-                    localStorage.setItem(
-                        'affiliatedata',
-                        JSON.stringify(affiliateData)
-                    );
-                }
-
-                if (users[0].role === 'admin') {
-                    let { data: userInfo, error: errorFetchUserInfo } =
-                        await supabase
-                            .from(`admins`)
-                            .select('name, lastname, root')
-                            .eq('dni', users[0].dni);
-
-                    if (errorFetchUserInfo) {
-                        console.log(errorFetchUserInfo);
-                        setLoading(false);
-                        return alert('Error en fetch user info.');
-                    }
-
-                    if (users[0].role === 'medic') {
-                        let { data: userInfo, error: errorFetchUserInfo } =
-                            await supabase
-                                .from(`medics`)
-                                .select(
-                                    'dni, name, lastname, medic_license, email, phone_number, profilePic, birthdate,  medical_specialities (id, name), medics_partners(partner_dni)'
-                                )
-                                .eq('dni', users[0].dni);
-
-                        if (errorFetchUserInfo) {
-                            console.log(errorFetchUserInfo);
-                            setLoading(false);
-                            return alert('Error en fetch user info.');
-                        }
-                        console.log(userInfo);
-                        const medicdata = {
-                            dni: userInfo[0].dni,
-                            name: userInfo[0].name,
-                            lastname: userInfo[0].lastname,
-                            medic_license: userInfo[0].medic_license,
-                            medical_specialities:
-                                userInfo[0].medical_specialities.map(
-                                    (el) => el
-                                ),
-                            my_patients: userInfo[0].medics_partners.map(
-                                (el) => el
-                            ),
-                            email: userInfo[0].email,
-                            phone_number: userInfo[0].phone_number,
-                            profilePic: userInfo[0].profilePic,
-                            birthdate: userInfo[0].birthdate,
-                        };
-                        localStorage.setItem(
-                            'medicdata',
-                            JSON.stringify(medicdata)
-                        );
-                    }
-
-                    await firebase
-                        .auth()
-                        .signInWithEmailAndPassword(users[0].email, input.pass);
-
-                    await firebase
-                        .auth()
-                        .signInWithEmailAndPassword(users[0].email, input.pass);
-
-                    localStorage.setItem('userdata', JSON.stringify(userData));
-                    window.location = `/${userData.dni}/${userData.role}`;
-                    setLoading(false);
-                }
+                setErrors(null);
             }
+
+            if (users[0].role === 'affiliate') {
+                let { data: userInfo, error: errorFetchUserInfo } =
+                    await supabase
+                        .from(`partners`)
+                        .select(
+                            'name, lastname, family_group, plans (id, name)'
+                        )
+                        .eq('dni', users[0].dni);
+
+                if (errorFetchUserInfo) {
+                    console.log(errorFetchUserInfo);
+                    setLoading(false);
+                    return alert('Error en fetch user info.');
+                }
+
+                const affiliateData = {
+                    name: userInfo[0].name,
+                    lastname: userInfo[0].lastname,
+                    plan_id: userInfo[0].plans.id,
+                    plan_name: userInfo[0].plans.name,
+                    family_group: userInfo[0].family_group,
+                };
+                localStorage.setItem(
+                    'affiliatedata',
+                    JSON.stringify(affiliateData)
+                );
+            }
+
+            if (users[0].role === 'admin') {
+                let { data: userInfo, error: errorFetchUserInfo } =
+                    await supabase
+                        .from(`admins`)
+                        .select('name, lastname, root')
+                        .eq('dni', users[0].dni);
+
+                if (errorFetchUserInfo) {
+                    console.log(errorFetchUserInfo);
+                    setLoading(false);
+                    return alert('Error en fetch user info.');
+                }
+
+                const adminData = {
+                    name: userInfo[0].name,
+                    lastname: userInfo[0].lastname,
+                    root: userInfo[0].root,
+                };
+                localStorage.setItem('admindata', JSON.stringify(adminData));
+            }
+
+            if (users[0].role === 'medic') {
+                let { data: userInfo, error: errorFetchUserInfo } =
+                    await supabase
+                        .from(`medics`)
+                        .select(
+                            'dni, name, lastname, medic_license, email, phone_number, profilePic, birthdate,  medical_specialities (id, name), medics_partners(partner_dni)'
+                        )
+                        .eq('dni', users[0].dni);
+
+                if (errorFetchUserInfo) {
+                    console.log(errorFetchUserInfo);
+                    setLoading(false);
+                    return alert('Error en fetch user info.');
+                }
+                console.log(userInfo);
+                const medicdata = {
+                    dni: userInfo[0].dni,
+                    name: userInfo[0].name,
+                    lastname: userInfo[0].lastname,
+                    medic_license: userInfo[0].medic_license,
+                    medical_specialities: userInfo[0].medical_specialities.map(
+                        (el) => el
+                    ),
+                    my_patients: userInfo[0].medics_partners.map((el) => el),
+                    email: userInfo[0].email,
+                    phone_number: userInfo[0].phone_number,
+                    profilePic: userInfo[0].profilePic,
+                    birthdate: userInfo[0].birthdate,
+                };
+                localStorage.setItem('medicdata', JSON.stringify(medicdata));
+            }
+
+            await firebase
+                .auth()
+                .signInWithEmailAndPassword(users[0].email, input.pass);
+
+            const dataUser = {
+                dni: users[0].dni,
+                email: users[0].email,
+                role: users[0].role,
+                avatar_url: users[0].avatar_url,
+            };
+
+            localStorage.setItem('userdata', JSON.stringify(dataUser));
+            window.location = `/${userData.dni}/${userData.role}`;
+            setLoading(false);
         } catch (error) {
             setErrors('Usuario y/o contraseña incorrecto.');
             setLoading(false);
@@ -352,9 +336,9 @@ function Login({ firebase }) {
                             onChange={handleInput}
                             required
                         />
-                        <a href='#!' className={styles.a} onClick={handleAlert}>
+                        <button className={styles.a} onClick={handleAlert}>
                             ¿Olvidaste tu contraseña?
-                        </a>
+                        </button>
                         {errors ? (
                             <p className={styles.warningLogin}>
                                 Usuario y/o contraseña incorrecta
