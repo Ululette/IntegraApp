c20904495653dea003ce23a622b40c8958915a39
Julian Vazquez <julianvazquez042@gmail.com>
2021-05-29 12:13:36 -0300

    Admin home finish

--- client/src/components/Admin/AdminHome/AdminHome.jsx
+++ client/src/components/Admin/AdminHome/AdminHome.jsx
@@ -5,6 +5,19 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import Chart from 'react-google-charts';
 import CountUp from 'react-countup';
 
+//MATERIAL-UI COMPONENTS
+import { DataGrid } from '@material-ui/data-grid';
+
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import AccessibilityIcon from '@material-ui/icons/Accessibility';
+import ErrorIcon from '@material-ui/icons/Error';
+import AddBoxIcon from '@material-ui/icons/AddBox';
+import SettingsIcon from '@material-ui/icons/Settings';
+import AddToQueueIcon from '@material-ui/icons/AddToQueue';
+import HourglassEmptyIcon from '@material-ui/icons/HourglassEmpty';
+import WarningIcon from '@material-ui/icons/Warning';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
 function AdminHome() {
     const adminData = JSON.parse(localStorage.getItem('admindata'));
 
@@ -17,6 +30,7 @@ function AdminHome() {
     const [planInfo, setPlanInfo] = useState('');
     const [admins, setAdmins] = useState('');
     const [authorizations, setAuthorizations] = useState('');
+    const [debtors, setDebtors] = useState('');
 
     const fetchingData = async () => {
         const { data: numberAffiliates } = await supabase
@@ -59,6 +73,30 @@ function AdminHome() {
 
         setContactForm(numberContacts.length);
 
+        //FECHAS
+
+        const rightNow = new Date();
+        const monthFormated =
+            String(rightNow.getMonth()).length === 1
+                ? `0${rightNow.getMonth()}`
+                : `${rightNow.getMonth()}`;
+        const dayFormated =
+            String(rightNow.getDate()).length === 1
+                ? `0${rightNow.getDate()}`
+                : `${rightNow.getDate()}`;
+
+        const dateFormated = `${rightNow.getFullYear()}-${monthFormated}-${dayFormated}`;
+
+        const { data: paymentsExpired } = await supabase
+            .from('payments')
+            .select(
+                'expiration_date, amount, payed, partners(dni, name, lastname)'
+            )
+            .lt('expiration_date', dateFormated)
+            .eq('payed', false);
+
+        setDebtors(paymentsExpired);
+
         const { data: numberPlans } = await supabase
             .from('partners')
             .select('plans (name, price)')
@@ -97,61 +135,165 @@ function AdminHome() {
         countPlans.push({ name: key, count: counts[key] });
     }
 
+    const columns = [
+        { field: 'id', headerName: 'ID', width: 100 },
+        { field: 'dni', headerName: 'DNI', width: 200 },
+        { field: 'name', headerName: 'Nombre', width: 200 },
+        { field: 'lastname', headerName: 'Apellido', width: 200 },
+        {
+            field: 'month',
+            headerName: 'Fecha expiracion',
+            width: 200,
+        },
+        {
+            field: 'debt',
+            headerName: 'Deuda',
+            width: 200,
+        },
+    ];
+
+    const rows = debtors.map((el, index) => {
+        return {
+            id: index,
+            dni: el.partners.dni,
+            name: el.partners.name,
+            lastname: el.partners.lastname,
+            month: el.expiration_date,
+            debt: el.amount,
+        };
+    });
+
+    console.log(rows);
+
     return (
         <div className={styles.container}>
             <h1>Hola {adminData.name}</h1>
             <div className={styles.dataContainer}>
-                <article>
-                    <h3>
-                        Afiliados activos{' '}
-                        <CountUp end={activeAffiliates} duration={5} />
-                    </h3>
-                </article>
-                <article>
-                    <h3>
-                        Interesados a revisar{' '}
-                        <CountUp end={affiliatesToRevision} duration={5} />
-                    </h3>
-                </article>
-                <article>
-                    <h3>
-                        Formularios pendientes{' '}
-                        <CountUp end={contactForm} duration={5} />
-                    </h3>
-                </article>
-                <article>
-                    <h3>
-                        Pedidos de bajas{' '}
-                        <CountUp end={downRequests} duration={5} />
-                    </h3>
-                </article>
-                <article>
-                    <h3>
-                        Medicos activos <CountUp end={medics} duration={5} />
-                    </h3>
-                </article>
-                <article>
-                    <h3>
-                        Administradores <CountUp end={admins} duration={5} />
-                    </h3>
-                </article>
+                <section className={styles.card}>
+                    <div className={styles.iconCard}>
+                        <StorefrontIcon />
+                    </div>
+                    <div className={styles.infoCard}>
+                        <div className={styles.auxiliar}></div>
+                        <article className={styles.description}>
+                            <p>Ganancias mensuales</p>
+                            <h3>
+                                $<CountUp end={planInfo} duration={10} />
+                            </h3>
+                        </article>
+                    </div>
+                </section>
+                <section className={styles.card}>
+                    <div className={styles.iconCard}>
+                        <AddToQueueIcon />
+                    </div>
+                    <div className={styles.infoCard}>
+                        <div className={styles.auxiliar}></div>
+                        <article className={styles.description}>
+                            <p>Interesados a revisar</p>
+                            <h3>
+                                <CountUp
+                                    end={affiliatesToRevision}
+                                    duration={5}
+                                />
+                            </h3>
+                        </article>
+                    </div>
+                </section>
+                <section className={styles.card}>
+                    <div className={styles.iconCard}>
+                        <HourglassEmptyIcon />
+                    </div>
+                    <div className={styles.infoCard}>
+                        <div className={styles.auxiliar}></div>
+                        <article className={styles.description}>
+                            <p>Faltan enviar formulario</p>
+                            <h3>
+                                <CountUp end={contactForm} duration={5} />
+                            </h3>
+                        </article>
+                    </div>
+                </section>
+                <section className={styles.card}>
+                    <div className={styles.iconCard}>
+                        <ErrorIcon />
+                    </div>
+                    <div className={styles.infoCard}>
+                        <div className={styles.auxiliar}></div>
+                        <article className={styles.description}>
+                            <p>Socios que quieren la baja</p>
+                            <h3>
+                                <CountUp end={downRequests} duration={5} />
+                            </h3>
+                        </article>
+                    </div>
+                </section>
+                <section className={styles.card}>
+                    <div className={styles.iconCard}>
+                        <AccessibilityIcon />
+                    </div>
+                    <div className={styles.infoCard}>
+                        <div className={styles.auxiliar}></div>
+                        <article className={styles.description}>
+                            <p>Afiliados activos</p>
+                            <h3>
+                                <CountUp end={activeAffiliates} duration={5} />
+                            </h3>
+                        </article>
+                    </div>
+                </section>
+                <section className={styles.card}>
+                    <div className={styles.iconCard}>
+                        <AddBoxIcon />
+                    </div>
+                    <div className={styles.infoCard}>
+                        <div className={styles.auxiliar}></div>
+                        <article className={styles.description}>
+                            <p>Medicos activos</p>
+                            <h3>
+                                <CountUp end={medics} duration={5} />
+                            </h3>
+                        </article>
+                    </div>
+                </section>
+                <section className={styles.card}>
+                    <div className={styles.iconCard}>
+                        <SettingsIcon />
+                    </div>
+                    <div className={styles.infoCard}>
+                        <div className={styles.auxiliar}></div>
+                        <article className={styles.description}>
+                            <p>Administradores</p>
+                            <h3>
+                                <CountUp end={admins} duration={5} />
+                            </h3>
+                        </article>
+                    </div>
+                </section>
+            </div>
+            <div className={styles.dataGridContainer}>
+                <div className={styles.iconGrid}>
+                    <WarningIcon />
+                    <h3>Socios con deudas</h3>
+                </div>
+                <section className={styles.dataGrid}>
+                    <DataGrid rows={rows} columns={columns} pageSize={5} />
+                </section>
+            </div>
+            <section className={styles.chartContainer}>
+                <div className={styles.iconChart}>
+                    <ShoppingCartIcon />
+                    <h3>Planes mas consumidos</h3>
+                </div>
                 <Chart
-                    width={'500px'}
-                    height={'300px'}
                     chartType='PieChart'
                     loader={<CircularProgress />}
                     data={countPlans.map((el) =>
                         el.name ? [el.name, el.count] : el
                     )}
-                    options={{ title: 'Planes mas consumidos' }}
+                    className={styles.chart}
                 />
-                <article>
-                    <h3>
-                        Ganancias mensuales $
-                        <CountUp end={planInfo} duration={10} />
-                    </h3>
-                </article>
-            </div>
+            </section>
         </div>
     );
 }
