fe398026c8869d8cbfc8e1b39749619f0b2d2340
Julian Vazquez <julianvazquez042@gmail.com>
2021-06-04 16:44:39 -0300

    Update navBar.jsx

--- client/src/components/Guest/NavBar/navBar.jsx
+++ client/src/components/Guest/NavBar/navBar.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import styles from './navBar.module.css';
 import logoNav from '../../../assets/images/logo.png';
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-const NavBar = ({ history }) => {
-    const location = history.location.pathname;
+const NavBar = () => {
+    const myRef = useSelector((state) => state.plans.refContact);
 
-    return location === '/login' || location.includes('admin') ? null : (
+    const scrollToBottom = () => {
+        myRef.current.scrollIntoView();
+    };
+
+    return (
         <nav className={styles.bar}>
             <NavLink className={styles.logo} to='/'>
                 <img className={styles.img} src={logoNav} alt='Logo.' />
@@ -15,18 +20,11 @@ const NavBar = ({ history }) => {
                 <NavLink to='/faqs'>
                     <button className={styles.logB}>FAQS</button>
                 </NavLink>
-                {location.includes('faqs')?null:(
-                    <button className={styles.logB}>
-                        <a className={styles.linkAsociate} href='#contact'>
-                            Asociate
-                        </a>
-                    </button>
-                )}
+                <button className={styles.logB} onClick={scrollToBottom}>
+                    Asociate
+                </button>
                 <NavLink to='/login' className={styles.navL}>
                     <button className={styles.login}>Entrar</button>
-                </NavLink>
-                <NavLink to='/about' className={styles.navL}>
-                    <button className={styles.login}>About</button>
                 </NavLink>
             </div>
         </nav>
